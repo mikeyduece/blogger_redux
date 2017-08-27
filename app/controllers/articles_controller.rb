71f8825a4abb4cd594324a3ba8408969a1d0434b
Mike Heft <mikeheft@gmail.com>
2017-08-27 16:24:29 -0600

    add rest of crud actions/views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show]
+  before_action :set_article, only: [:show, :destroy, :edit, :update]
   def index
     @articles = Article.all
   end
@@ -7,8 +7,34 @@ class ArticlesController < ApplicationController
   def show
   end
   
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    @article = Article.create(article_params)
+    flash.notice = "Article '#{@article.title} Updated!'"
+    redirect_to article_path(@article)
+  end
+  
+  def edit
+  end
+  
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+  
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+  
   private
   
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
   def set_article
     @article = Article.find(params[:id])
   end
