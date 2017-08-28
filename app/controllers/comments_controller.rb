f30d6e0dcf5e4b3e1ef533f77256ff73c4192193
Mike Heft <mikeheft@gmail.com>
2017-08-28 07:10:55 -0600

    fix typo in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
-    @comment.article_id = params[:article]
+    @comment.article_id = params[:article_id]
     @comment.save
     redirect_to article_path(@comment.article)
   end
